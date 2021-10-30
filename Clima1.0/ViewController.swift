6ed3be555f45d410f40f5987dcb10621faba8ea5
Mero <arsmohamed@outlook.com>
2021-10-30 13:13:57 -0230

    Changed the icons, the design and added some styling and working on the vertical scrolling for the data coming about the daily weather

--- Clima1.0/ViewController.swift
+++ Clima1.0/ViewController.swift
@@ -8,10 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //SearchBar
+    @IBOutlet weak var searchBar: UITextField!
+    //Containers View
+    @IBOutlet weak var topContainerV: UIView!
+    @IBOutlet weak var middleContainerV: UIView!
+    @IBOutlet weak var bottomContainerV: UIView!
+    //Location
+    @IBOutlet weak var Location: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //Containers round edges
+        topContainerV.layer.cornerRadius = 21
+        middleContainerV.layer.cornerRadius = 21
+        bottomContainerV.layer.cornerRadius = 21
+        //this to allow the edges to cut
+        topContainerV.layer.masksToBounds = true
+        middleContainerV.layer.masksToBounds = true
+        bottomContainerV.layer.masksToBounds = true
+        //placeholder coloer
+        searchBar.attributedPlaceholder = NSAttributedString(
+            string: "Search",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor.lightGray]
+        )
     }
 
 
